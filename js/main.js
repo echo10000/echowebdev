2dea863e3d29a4b082f447bf5a06613bde3cb8f9
echo10000 <echogoodkid@gmail.com>
2025-09-23 16:30:32 +0800

    jericho gay

---
+++ js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function () {
+    "use strict"
+ 
+    $(".text").typed({
+        strings: ["<strong> I Love you </strong><strong class='primary'> Baby. </strong>", 
+            "<strong> mwuahh</strong><strong class= 'primary'> mwuahhh!!.</strong>"],
+        typespeed: 0,
+        loop: true
+    });
+
+});
