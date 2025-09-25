9b334341ae481f9ae2739f301c863cac519322fb
echo10000 <echogoodkid@gmail.com>
2025-09-25 16:07:21 +0800

    webdev

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 jQuery(document).ready(function () {
-    "use strict"
+    "use strict";
  
     $(".text").typed({
         strings: ["<strong> I Love you </strong><strong class='primary'> Baby. </strong>", 
-            "<strong> mwuahh</strong><strong class= 'primary'> mwuahhh!!.</strong>"],
-        typespeed: 0,
+            "<strong> mwuahh</strong><strong class='primary'> mwuahhh!!.</strong>"],
+        typeSpeed: 50,
         loop: true
-
-        });
+    });
 
     $(".slider").ripples({
         dropRadius: 11,
-        pertubance: 0.01,
+        perturbance: 0.01,
     });
-}
-
-)
+});
